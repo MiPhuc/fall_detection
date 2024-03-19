f9a55d2ba5de6b2f04e0e1e557bc10b839228a76
MiPhuc <miphuc1508@gmail.com>
2024-03-19 22:53:58 +0700

    change main.py

--- app/main.py
+++ app/main.py
@@ -4,8 +4,6 @@ from typing import List
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI, Depends, HTTPException
 
-import uvicorn
-
 app = FastAPI()
 class request_obj(BaseModel):
     sensor1 : List[int]
@@ -21,9 +19,3 @@ def predict(request :request_obj):
         pred = response_obj(predicted = False)
     return pred
     
-
-
-if __name__ == "__main__":
-    config = uvicorn.Config(app, port=5012, host = '192.168.1.11')
-    server = uvicorn.Server(config)
-    server.run()
